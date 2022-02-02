34f1bf19e2c0a3135443ebe7d07126cbff461476
Assaf Sapir <assapir@microsoft.com>
2022-02-02 14:18:08 +0000

    maybe now

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 if ! grep -q "codespace.*/bin/zsh" /etc/passwd; then
+  echo "Changing shell to zsh"
   sudo chsh -s /bin/zsh codespace
 fi
 
@@ -14,4 +15,9 @@ curl -fsSL https://starship.rs/install.sh | zsh
 
 # copy starship config
 mkdir -p $HOME/.config
-cp starship.toml ~/.config/starship.toml
+cp starship.toml $HOME/.config/starship.toml
+
+echo "==========================================================="
+echo "             import zshrc                                  "
+echo "-----------------------------------------------------------"
+cat .zshrc > $HOME/.zshrc
