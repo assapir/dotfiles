19c78b8d5095d7ca752b542d4a69f7d14f650378
Assaf Sapir <assapir@github.com>
2022-02-03 08:54:20 +0200

    remove startship

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,13 +10,6 @@ fi
 
 export BUNDLE_RUBYGEMS__PKG__GITHUB__COM=$GITHUB_TOKEN
 
-# install starship
-curl -fsSL https://starship.rs/install.sh | zsh
-
-# copy starship config
-mkdir -p $HOME/.config
-cp starship.toml $HOME/.config/starship.toml
-
 echo "==========================================================="
 echo "             import zshrc                                  "
 echo "-----------------------------------------------------------"
